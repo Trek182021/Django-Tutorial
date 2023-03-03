6225bc1f78edada703f07fb836d0954332c6cd73
Trek182021 <82979983+Trek182021@users.noreply.github.com>
2023-03-03 09:58:42 -0800

    Added Sidebar, Bootstrap, Registration, and Crispy Forms.

--- pTest/main/views.py
+++ pTest/main/views.py
@@ -31,7 +31,9 @@ def index(response, id):
     return render(response, 'main/list.html', {"ls": ls})
 
 def home(response):
-    return render(response, 'main/home.html', {})
+    lists = ToDoList.objects.all()
+    
+    return render(response, 'main/home.html', {"lists": lists})
 
 def create(response):
     if response.method == 'POST':
