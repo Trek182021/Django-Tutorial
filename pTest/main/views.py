4f7671549e39e7fb2729666c1803c1a1f941b534
Trek182021 <82979983+Trek182021@users.noreply.github.com>
2023-03-02 22:48:11 -0800

    Added Templates and Base HTML.

--- pTest/main/views.py
+++ pTest/main/views.py
@@ -4,7 +4,9 @@ from .models import ToDoList, Item
 
 # Create your views here.
 
-def index(response, name):
-    ls = ToDoList.objects.get(id=1)
-    item = ls.item_set.create(text = name, complete=False)
-    return HttpResponse("test successful! %s " % item)
+def index(response, id):
+    ls = ToDoList.objects.get(id=id)
+    return render(response, 'main/list.html', {"ls": ls})
+
+def home(response):
+    return render(response, 'main/home.html', {})
