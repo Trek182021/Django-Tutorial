123ddf7c35bbcadf2e4fdf7d907dcbf66d3c5bcc
Trek182021 <82979983+Trek182021@users.noreply.github.com>
2023-03-04 09:03:33 -0800

    Update

--- pTest/main/urls.py
+++ pTest/main/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns =[
     path("<int:id>", views.index, name="index"),
-    path("home/", views.home, name="home") ,
+    path("", views.home, name="home") ,
     path("create/", views.create, name="create"),
+    path("view/", views.view, name="view"),
 ]
--- pTest/pTest/settings.py
+++ pTest/pTest/settings.py
@@ -119,8 +119,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
-
+STATIC_URL = '/static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
@@ -130,3 +129,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+
+LOGIN_REDIRECT_URL = "/"
+
+LOGOUT_REDIRECT_URL = "/"
